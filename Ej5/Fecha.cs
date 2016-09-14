43433b12436a15a14f7ed9d0fbf23f9136b59ead
Aye <Aye@AYE-PC>
2016-09-14 19:23:27 -0300

    Ejemplo tp5

--- Ej5/Fecha.cs
+++ Ej5/Fecha.cs
@@ -8,58 +8,47 @@ namespace Ej5
 {
     class Fecha
     {
-        private readonly string iDia;
-        private readonly string iMes;
-        private readonly string iAnio;
-
-        public string AgregarDia (string pDia)
-        {
-            
-
-        }
-        public string idias { get; }
-        public string imes { get; }
-        public string ianio { get; }
-
-        private const int Anio_Base = 1900;
-        private const int Anio_Max = 2499;
-        private static readonly int[] Cant_Dias_Mes = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        private static readonly int[] ClaveMes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         private readonly long iCantidadDias;
         private readonly int iDia;
         private readonly int iMes;
-        private readonly int iAnio;
+        private readonly int iAño;
+
+        private const int Año_Base = 1900;
+        private const int Año_Max = 2499;
+        private static readonly int[] Cant_Dias_Mes = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        private static readonly int[] ClaveMes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        
         // public Fechas(int pDia , int pMes, int pAnio);
         //Metodo
-        private static int CalcularCantidadDiasMes(int pDias, int pMes, int pAnio)
+        private static int CalcularCantidadDiasMes(int pDia, int pMes, int pAño)
         {
-            if (pDias)
+            if (pDia)
             { int mCantidadDias -= 1; }
 
         }
-        public Fechas AgregarDias(int pCantidadDias)
+        public Fecha AgregarDias(int pCantidadDias)
         {
             return
         }
 
-        public Fechas(string fdias, string fmes, string fanio)//Constructor.
+        public Fecha(int fdia, int fmes, int faño)//Constructor.
         {
-            idias = fdias;
-            imes = fmes;
-            ianio = fanio;
+           this.iDia = fdia;
+            this.iMes = fmes;
+            this.iAño = faño;
         }
 
         public string dias//Propiedad.
         {
-            get { return this.idias; }
+            get { return this.iDia; }
         }
         public string mes//Propiedad.
         {
-            get { return this.imes; }
+            get { return this.iMes ; }
         }
-        public string anio//Propiedad.
+        public string año//Propiedad.
         {
-            get { return this.ianio; }
+            get { return this.iAño; }
         }
         //string.Format nos permita formatea una cadena de caracteres .
         public int CompararFecha(Fechas Fech)
@@ -67,15 +56,15 @@ namespace Ej5
 
         }
 
-        public string agregardia(string dia) //Metodo.
+        public string agregardia(int dia) //Metodo.
         {
 
         }
-        public string agregarmes(string dia) //Metodo.
+        public string agregarmes(int mes) //Metodo.
         {
 
         }
-        public string agregaranio(string dia) //Metodo.
+        public string agregaraño(int año) //Metodo.
         {
 
         }
