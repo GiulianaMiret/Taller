b0b87e9d90cf4a5bec5583865e522344a81f9fec
GiulianaMiret <dariela_77@hotmail.com>
2016-09-14 19:34:55 -0300

    s

--- Ej5/Fecha.cs
+++ Ej5/Fecha.cs
@@ -8,14 +8,63 @@ namespace Ej5
 {
     class Fecha
     {
-        private readonly string iDia;
-        private readonly string iMes;
-        private readonly string iAnio;
-
-        public string AgregarDia (string pDia)
+        private const int Anio_Base = 1900;
+        private const int Anio_Max = 2499;
+        private static readonly int[] Cant_Dias_Mes = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        private static readonly int[] ClaveMes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        private static readonly String[] DIA_SEMANA = { "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" };
+        private readonly long iCantidadDias;
+        private readonly int iDia;
+        private readonly int iMes;
+        private readonly int iAnio;
+        // public Fechas(int pDia , int pMes, int pAnio);
+        //Metodo
+        private static int CalcularCantidadDiasMes(int pDias, int pMes, int pAnio)
         {
-            
+            if (pDias)
+            { int mCantidadDias -= 1; }
+
+        }
+        public Fechas AgregarDias(int pCantidadDias)
+        {
+			return;
+        }
+
+        public Fecha(string fdias, string fmes, string fanio)//Constructor.
+        {
+            this.iDias = fdias;
+            this.iMes = fmes;
+            this.iAnio = fanio;
+        }
+
+        public string dias//Propiedad.
+        {
+            get { return this.iDias; }
+        }
+        public string mes//Propiedad.
+        {
+            get { return this.iMes; }
+        }
+        public string anio//Propiedad.
+        {
+            get { return this.iAnio; }
+        }
+        //string.Format nos permita formatea una cadena de caracteres .
+        public int CompararFecha(Fechas Fech)
+        {
+
+        }
+
+        public string agregardia(string dia) //Metodo.
+        {
+
+        }
+        public string agregarmes(string dia) //Metodo.
+        {
+
+        }
+        public string agregaranio(string dia) //Metodo.
+        {
 
         }
     }
-}
